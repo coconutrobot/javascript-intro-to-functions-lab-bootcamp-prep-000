81913b4c8c4385ea452756540048a181402bcf6f
Kacia Hosmer <kacia@coconutrobot.com>
2018-05-17 19:27:30 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,27 @@
+function shout(string) { 
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+var spy = "hello"
+var uppercase = "HELLO"
+var lowercase = "hello"
+
+function logWhisper(string) {
+  console.log(spy)
+  return string.toLowerCase()
+}
+
+function logShout(string) {
+  console.log(uppercase)
+  return string.toUpperCase()
+}
+
+function sayHiToGrandma(string) {
+  if ('hello')
+  return ("I can't hear you!")
+}
+
